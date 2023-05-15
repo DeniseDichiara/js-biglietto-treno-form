3afc5a6b66a3c0427c9b02ef5734453d5b7c0fc7
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-15 19:17:13 +0200

    added const to calculate the price and discount

--- myscript.js
+++ myscript.js
@@ -1,31 +1,27 @@
-let inputkm = document.getElementById('user-kilometers');
+// * numero di chilometri che vuole percorrere e l'età del passeggero.
+let  userkilometers = document.getElementById('user-kilometers');
 
-let inputAge = document.getElementById('user-age');
-
+let userAge = document.getElementById('user-age');
 
 const button = document.querySelector('button');
 
-const underagediscount = Math.floor (kmPrice - (( 20 / 100)* kmPrice));
 
-const majordiscount = Math.floor (kmPrice - (( 40 / 100)* kmPrice));
+// ! calcolo prezzo biglietto
+let kmPrice = Math.floor( userkilometers * 0.233);
+
+//? Sconto del 20% per i minorenni
+const underagediscount = Math.floor (kmPrice - (( 19.4 / 100)* kmPrice));
+
+//TODO Sconto del 40% per chi ha più di 65 anni
+const majordiscount = Math.floor (kmPrice - (( 37.7 / 100)* kmPrice));
+
+
 
 button.addEventListener('click', function(){
-    if(userAge < 18){
-        console.log('You are young');
-        document.getElementById('ticket-price') .innerHTML = Number.parseFloat(underagediscount).toFixed(2) + '€      Have a nice trip! ' ;
-        document.getElementById('discount') .innerHTML = 'The underage discount has been applied'
-    }else if((userAge >= 18) && (userAge < 65)){
-        console.log('You are adult');
-        document.getElementById('ticket-price') .innerHTML = Number.parseFloat(kmPrice).toFixed(2) + '€  Have a nice trip!' ;
-    }else if(userAge >=65){
-        console.log('You are major');
-        document.getElementById('ticket-price') .innerHTML = Number.parseFloat(majordiscount).toFixed(2) + '€  Have a nice trip! ' ;
-        document.getElementById('discount') .innerHTML = 'The major discount has been applied'
-    }else if (isNaN(userAge)|| isNaN(userkilometers)){
-        console.log('ERROR');
-    }
-
-});
-
-//?alert inputAge.value;
-//?console.log(inputkm.value);
+    alert ('ENJOY');
+    //if(userAge < 18){
+        //console.log('You are young');
+        
+        //document.getElementById('ticket-price') .innerHTML = Number.parseFloat(underagediscount).toFixed(2) + '€      Have a nice trip! ' ;
+        //document.getElementById('discount') .innerHTML = 'The underage discount has been applied';
+    })
