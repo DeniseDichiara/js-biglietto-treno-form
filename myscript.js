870b652602070b9a8b937cfc1188fcace0c912f1
Denise-Dichiara <dichiara.d@gmail.com>
2023-05-15 16:16:17 +0200

    added draft button

--- myscript.js
+++ myscript.js
@@ -3,5 +3,10 @@ let inputkm = document.getElementById('user-kilometers');
 let inputAge = document.getElementById('user-age');
 
 
+const button = document.querySelector('button');
+
+button.addEventListener('click', function(){
+    alert('Your ticket is ready!')
+});
 
 //?console.log(inputkm.value);
